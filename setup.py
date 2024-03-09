46d530ec440302a9c8b9044755669f23c61343da
Sakshi Choube <sakshichoube4232gmail.com>
2024-03-09 10:04:33 +0530

    updated file

--- setup.py
+++ setup.py
@@ -18,5 +18,6 @@ setup(
     version='0.0.1',
     author='sunny savita',
     author_email='sunny.savita@ineuron.ai',
+    install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
