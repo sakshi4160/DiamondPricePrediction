57829c6088daafc5db07505b2bf2fac182b2066b
Sakshi Choube <sakshichoube4232gmail.com>
2024-03-11 14:08:47 +0530

    code updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,5 +1,10 @@
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 
+from src.DimondPricePrediction.components.data_transformation import DataTransformation
+
+from src.DimondPricePrediction.components.model_trainer import ModelTrainer
+
+
 import os
 import sys
 from src.DimondPricePrediction.logger import logging
@@ -8,5 +13,11 @@ import pandas as pd
 
 obj=DataIngestion()
 
-obj.initiate_data_ingestion()
+train_data_path,test_data_path=obj.initiate_data_ingestion()
 
+data_transformation = DataTransformation()
+
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
+model_trainer=ModelTrainer()
+model_trainer.initate_model_training(train_arr,test_arr)
